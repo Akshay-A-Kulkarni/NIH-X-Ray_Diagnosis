ea063543ac25d22c596a563cc5cd4899df1b47ae
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2019-11-09 17:11:58 -0500

    Add files via upload
    Turned Geoff's Preprocessor, x_ray.py, into a class.  If your python file is saved to the same directory, you can now import it into other files by running a line of code similar to "from x_ray import preprocessor".   Like with other python packages, you can import it under a pseudonym by adding something like "as pp" to the end.

--- x_ray.py
+++ x_ray.py
@@ -5,70 +5,91 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
-def prepare_x_ray_data(csv_path, image_path, labels, img_width, img_height):
+class preprocessor: 
+    ''' 
+    Pre-processes image data into a 2D matrix format acceptable to most ML frameworks 
+    provided by Sci-Kit Learn. 
+    
+    Also ensures that processed data is standardized by mean.  
+    '''
+    def __init__(self, csv_path, image_path, labels, img_width, img_height):
+        self.csv_path = csv_path
+        self.image_path = image_path
+        self.labels = labels
+        self.img_width = img_width
+        self.img_height = img_height
+        
+        
+    def prepare_x_ray_data(self):
+    
+        # Load file from path and drop NA values
+        x_ray_dev = pd.read_csv(self.csv_path, header = 0)
+        x_ray_dev.dropna(inplace=True)
+    
+        # Remove rows with more than one Finding Label
+        x_ray_dev = x_ray_dev[~x_ray_dev['Finding Labels'].str.contains('\\|')]
+    
+        # Create list of Finding Labels
+        finding_Labels = sorted(x_ray_dev['Finding Labels'].unique())
+    
+        # Assign numerical Finding Labels to Finding IDs
+        label_IDs = {}
+        count = 1
+        for i in finding_Labels:
+            label_IDs[i] = count
+            count = count + 1
+    
+        # Filter for only the selected input labels
+        x_ray_dev = x_ray_dev[x_ray_dev['Finding Labels'].isin(self.labels)]
+    
+        # Replace Finding Labels with Finding IDs
+        x_ray_dev = x_ray_dev.replace({'Finding Labels': label_IDs})
+    
+        # Drop irrelevant/unused columns
+        x_ray_dev = x_ray_dev.drop(x_ray_dev.columns[range(2, 11)], axis=1)
+    
+        # Put the Image Index and Finding ID in an iterable list
+        result = [(x, y) for x, y in zip(x_ray_dev['Image Index'], x_ray_dev['Finding Labels'])]
+    
+        # Create a data frame of the flat image matrices and associated labels, and shuffle
+        training_data = []
+    
+        for item in result:
+            file = item[0]
+            label = item[1]
+            path = os.path.join(self.image_path, file)
+            img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  # load the image as a matrix
+            img = cv2.resize(img, (self.img_width, self.img_height))  # resize the image / matrix
+            img = img.flatten()  # flatten the matrix
+            data = np.append(img, label)  # combine the image matrix and label
+            training_data.append(data)
+    
+        image_data = pd.DataFrame(training_data).sample(frac=1)
+    
+        X, y = image_data.iloc[:, :-1], image_data.iloc[:, -1]
+    
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+    
+        scaler = preprocessing.StandardScaler()
+        X_train_s = scaler.fit_transform((X_train.values))
+        X_train = pd.DataFrame(X_train_s, index=X_train.index, columns=X_train.columns)
+        X_val_s = scaler.fit_transform((X_val.values))
+        X_val = pd.DataFrame(X_val_s, index=X_val.index, columns=X_val.columns)
+        X_test_s = scaler.fit_transform((X_test.values))
+        X_test = pd.DataFrame(X_test_s, index=X_test.index, columns=X_test.columns)
+    
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
-    # Load file from path and drop NA values
-    x_ray_dev = pd.read_csv(csv_path, header = 0)
-    x_ray_dev.dropna(inplace=True)
-
-    # Remove rows with more than one Finding Label
-    x_ray_dev = x_ray_dev[~x_ray_dev['Finding Labels'].str.contains('\\|')]
-
-    # Create list of Finding Labels
-    finding_Labels = sorted(x_ray_dev['Finding Labels'].unique())
-
-    # Assign numerical Finding Labels to Finding IDs
-    label_IDs = {}
-    count = 1
-    for i in finding_Labels:
-        label_IDs[i] = count
-        count = count + 1
-
-    # Filter for only the selected input labels
-    x_ray_dev = x_ray_dev[x_ray_dev['Finding Labels'].isin(labels)]
-
-    # Replace Finding Labels with Finding IDs
-    x_ray_dev = x_ray_dev.replace({'Finding Labels': label_IDs})
-
-    # Drop irrelevant/unused columns
-    x_ray_dev = x_ray_dev.drop(x_ray_dev.columns[range(2, 11)], axis=1)
-
-    # Put the Image Index and Finding ID in an iterable list
-    result = [(x, y) for x, y in zip(x_ray_dev['Image Index'], x_ray_dev['Finding Labels'])]
-
-    # Create a data frame of the flat image matrices and associated labels, and shuffle
-    training_data = []
-
-    for item in result:
-        file = item[0]
-        label = item[1]
-        path = os.path.join(image_path, file)
-        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  # load the image as a matrix
-        img = cv2.resize(img, (img_width, img_height))  # resize the image / matrix
-        img = img.flatten()  # flatten the matrix
-        data = np.append(img, label)  # combine the image matrix and label
-        training_data.append(data)
-
-    image_data = pd.DataFrame(training_data).sample(frac=1)
-
-    X, y = image_data.iloc[:, :-1], image_data.iloc[:, -1]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
-
-    scaler = preprocessing.StandardScaler()
-    X_train_s = scaler.fit_transform((X_train.values))
-    X_train = pd.DataFrame(X_train_s, index=X_train.index, columns=X_train.columns)
-    X_val_s = scaler.fit_transform((X_val.values))
-    X_val = pd.DataFrame(X_val_s, index=X_val.index, columns=X_val.columns)
-    X_test_s = scaler.fit_transform((X_test.values))
-    X_test = pd.DataFrame(X_test_s, index=X_test.index, columns=X_test.columns)
-
-    return X_train, X_val, X_test, y_train, y_val, y_test
 
 # Use the function (provide a path, select input labels, and call the function)
-# csv_path = '/Users/gk/Desktop/sample/sample_labels.csv'
-# image_path = '/Users/gk/Desktop/sample/sample/images/'
-# input_labels = ['Pneumothorax', 'Effusion']
-# X_train, X_val, X_test, y_train, y_val, y_test = prepare_x_ray_data(csv_path, image_path, input_labels, 100, 100)
+#csv_path = 'C:/Users/stany/Desktop/sample_data/sample_labels.csv'  #C:\Users\stany\Desktop
+#image_path = 'C:/Users/stany/Desktop/sample_data/images/'
+#input_labels = ['Pneumothorax', 'Effusion']
+#
+##Create an instance of the class 
+#preprocessor = preprocessor(csv_path, image_path, input_labels, 100, 100)
+#
+#
+#X_train, X_val, X_test, y_train, y_val, y_test = preprocessor.prepare_x_ray_data()
 
