7d0adbbfc004e151f11a57f2cb023618039c83f4
Geoff <geoff.korb@protonmail.com>
2019-11-11 17:17:24 -0500

    update to labels and shuffle

--- x_ray.py
+++ x_ray.py
@@ -2,58 +2,58 @@ import os
 import cv2
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing
+from sklearn import preprocessing, metrics
 from sklearn.model_selection import train_test_split
 
-class preprocessor: 
-    ''' 
-    Pre-processes image data into a 2D matrix format acceptable to most ML frameworks 
-    provided by Sci-Kit Learn. 
-    
-    Also ensures that processed data is standardized by mean.  
+class preprocessor:
     '''
-    def __init__(self, csv_path, image_path, labels, img_width, img_height):
+    Pre-processes image data into a 2D matrix format acceptable to most ML frameworks
+    provided by Sci-Kit Learn.
+
+    Also ensures that processed data is standardized by mean.
+    '''
+    def __init__(self, csv_path, image_path, img_width, img_height, labels = None):
         self.csv_path = csv_path
         self.image_path = image_path
         self.labels = labels
         self.img_width = img_width
         self.img_height = img_height
-        
-        
+
     def prepare_x_ray_data(self):
-    
+
         # Load file from path and drop NA values
         x_ray_dev = pd.read_csv(self.csv_path, header = 0)
         x_ray_dev.dropna(inplace=True)
-    
+
         # Remove rows with more than one Finding Label
         x_ray_dev = x_ray_dev[~x_ray_dev['Finding Labels'].str.contains('\\|')]
-    
+
         # Create list of Finding Labels
         finding_Labels = sorted(x_ray_dev['Finding Labels'].unique())
-    
+
         # Assign numerical Finding Labels to Finding IDs
         label_IDs = {}
         count = 1
         for i in finding_Labels:
             label_IDs[i] = count
             count = count + 1
-    
+
         # Filter for only the selected input labels
-        x_ray_dev = x_ray_dev[x_ray_dev['Finding Labels'].isin(self.labels)]
-    
+        if self.labels is not None:
+            x_ray_dev = x_ray_dev[x_ray_dev['Finding Labels'].isin(self.labels)]
+
         # Replace Finding Labels with Finding IDs
         x_ray_dev = x_ray_dev.replace({'Finding Labels': label_IDs})
-    
+
         # Drop irrelevant/unused columns
         x_ray_dev = x_ray_dev.drop(x_ray_dev.columns[range(2, 11)], axis=1)
-    
+
         # Put the Image Index and Finding ID in an iterable list
         result = [(x, y) for x, y in zip(x_ray_dev['Image Index'], x_ray_dev['Finding Labels'])]
-    
+
         # Create a data frame of the flat image matrices and associated labels, and shuffle
         training_data = []
-    
+
         for item in result:
             file = item[0]
             label = item[1]
@@ -63,14 +63,14 @@ class preprocessor:
             img = img.flatten()  # flatten the matrix
             data = np.append(img, label)  # combine the image matrix and label
             training_data.append(data)
-    
-        image_data = pd.DataFrame(training_data).sample(frac=1)
-    
+
+        image_data = pd.DataFrame(training_data)#.sample(frac=1)
+
         X, y = image_data.iloc[:, :-1], image_data.iloc[:, -1]
-    
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
         X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
-    
+
         scaler = preprocessing.StandardScaler()
         X_train_s = scaler.fit_transform((X_train.values))
         X_train = pd.DataFrame(X_train_s, index=X_train.index, columns=X_train.columns)
@@ -78,18 +78,9 @@ class preprocessor:
         X_val = pd.DataFrame(X_val_s, index=X_val.index, columns=X_val.columns)
         X_test_s = scaler.fit_transform((X_test.values))
         X_test = pd.DataFrame(X_test_s, index=X_test.index, columns=X_test.columns)
-    
+
         return X_train, X_val, X_test, y_train, y_val, y_test
 
 
-# Use the function (provide a path, select input labels, and call the function)
-#csv_path = 'C:/Users/stany/Desktop/sample_data/sample_labels.csv'  #C:\Users\stany\Desktop
-#image_path = 'C:/Users/stany/Desktop/sample_data/images/'
-#input_labels = ['Pneumothorax', 'Effusion']
-#
-##Create an instance of the class 
-#preprocessor = preprocessor(csv_path, image_path, input_labels, 100, 100)
-#
-#
-#X_train, X_val, X_test, y_train, y_val, y_test = preprocessor.prepare_x_ray_data()
+
 
---
+++ x_ray_logistic_regression.py
@@ -0,0 +1,32 @@
+from x_ray import preprocessor
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+# Use the function (provide a path, select input labels, and call the function)
+csv_path = '/Users/gk/Desktop/sample/sample_labels.csv'  #C:\Users\stany\Desktop
+image_path = '/Users/gk/Desktop/sample/sample/images/'
+# input_labels = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema',
+#                 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'No Finding', 'Nodule',
+#                 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']
+
+# input_labels = ['Effusion','Pneumothorax']
+
+#Create an instance of the class
+preprocessor = preprocessor(csv_path, image_path, 100, 100)
+
+X_train, X_val, X_test, y_train, y_val, y_test = preprocessor.prepare_x_ray_data()
+
+logistic_model = LogisticRegression(solver='lbfgs',random_state=0,max_iter=300)
+logistic_model.fit(X_train,y_train)
+logistic_pred = logistic_model.predict(X_test)
+conf_matrix = metrics.confusion_matrix(y_test,logistic_pred)
+logistic_accuracy = metrics.accuracy_score(y_test,logistic_pred,normalize=True)
+print(logistic_accuracy)
+logistic_error = 1 - logistic_accuracy
+print(logistic_error)
+# logistic_precision = metrics.precision_score(y_test,logistic_pred)
+# print(logistic_precision)
+# logistic_recall = metrics.recall_score(y_test,logistic_pred)
+# print(logistic_recall)
+# logistic_f1 = metrics.f1_score(y_test,logistic_pred)
+# print(logistic_f1)
