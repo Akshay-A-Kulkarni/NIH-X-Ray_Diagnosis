1b4254406e4923bc4e57cef98d315ae2ea331301
Stanley Yu <49109653+stanley-c-yu@users.noreply.github.com>
2019-11-22 16:43:54 -0500

    Misc changes
    etc

--- x_ray_logistic_regression.py
+++ x_ray_logistic_regression.py
@@ -3,11 +3,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 
 # Use the function (provide a path, select input labels, and call the function)
-csv_path = '/Users/gk/Desktop/sample/sample_labels.csv'  #C:\Users\stany\Desktop
-image_path = '/Users/gk/Desktop/sample/sample/images/'
-# input_labels = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema',
-#                 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'No Finding', 'Nodule',
-#                 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']
+#csv_path = '/Users/gk/Desktop/sample/sample_labels.csv'  #C:\Users\stany\Desktop
+csv_path = 'C:/Users/stany/Desktop/project-data-repo/x_ray_1000.csv'
+#image_path = '/Users/gk/Desktop/sample/sample/images/'
+image_path = 'C:/Users/stany/Desktop/project-data-repo/x_ray_1000_images/'
+input_labels = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Effusion', 'Emphysema',
+                 'Fibrosis', 'Hernia', 'Infiltration', 'Mass', 'No Finding', 'Nodule',
+                 'Pleural_Thickening', 'Pneumonia', 'Pneumothorax']
 
 # input_labels = ['Effusion','Pneumothorax']
 
@@ -16,7 +18,7 @@ preprocessor = preprocessor(csv_path, image_path, 100, 100)
 
 X_train, X_val, X_test, y_train, y_val, y_test = preprocessor.prepare_x_ray_data()
 
-logistic_model = LogisticRegression(solver='lbfgs',random_state=0,max_iter=300)
+logistic_model = LogisticRegression(solver='lbfgs',random_state=123,max_iter=300)
 logistic_model.fit(X_train,y_train)
 logistic_pred = logistic_model.predict(X_test)
 conf_matrix = metrics.confusion_matrix(y_test,logistic_pred)
